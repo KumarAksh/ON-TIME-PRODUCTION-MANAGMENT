987a4244a9c7dac9ff5a4665251131ec43c0f9f1
KumarAksh <46560622+KumarAksh@users.noreply.github.com>
2019-01-13 20:28:26 +0530

    Add files via upload

---
+++ Cookie Factory/src/cookie/factory/Trucks.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cookie.factory;
+
+/**
+ *
+ * @author hp
+ */
+public class Trucks {
+    String TruckId;
+    String Content;
+    int Amount;
+}
